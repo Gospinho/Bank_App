23d387b9dc6b8f04552b6d5f737dd8735c174ac3
Gospinho <54376176+Gospinho@users.noreply.github.com>
2022-04-26 18:47:52 +0200

    add BankApp class and set for parameters and get and set for "name", "surname", "accNumber" params. Create Account method was created

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Account {
 
     private String name;
     private String surname;
     private String accNumber;
     private long balance;
+
+    Scanner scanner = new Scanner(System.in);
 
     public String getName() {
         return name;
@@ -25,8 +29,29 @@ public class Account {
         return accNumber;
     }
 
-    public void setAccNumber (String accNumber) {
+    public void setAccNumber(String accNumber) {
         this.accNumber = accNumber;
     }
 
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public void createAccount() {
+        System.out.print("Enter Your name: ");
+        name = scanner.next();
+
+        System.out.print("Enter Your surname: ");
+        surname = scanner.next();
+
+        System.out.print("Enter Your account number: ");
+        accNumber = scanner.next();
+
+        System.out.print("Enter Your balance: ");
+        balance = scanner.nextLong();
+    }
 }
