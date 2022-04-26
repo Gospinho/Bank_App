4eef15794becb85399be0942c4cff0af2721e6d9
Gospinho <54376176+Gospinho@users.noreply.github.com>
2022-04-26 20:43:47 +0200

    Created do while for app Created case 4 for exit fromm app

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -5,32 +5,37 @@ public class BankApp {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        Account account = new Account();
-
-        account.createAccount();
         System.out.println("Welcome in our bank!");
-        System.out.println("1. display account details");
-        System.out.println("2. Deposit money");
-        System.out.println("3. Take back Your money");
-        System.out.println("4. Exit app");
-        System.out.print("Enter Your choice: ");
+        Account account = new Account();
+        account.createAccount();
 
-        int inputNumber = scanner.nextInt();
+        int inputNumber;
 
-        switch (inputNumber) {
-            case 1: {
-                account.showAccount();
+        do {
+            System.out.println("1. display account details");
+            System.out.println("2. Deposit money");
+            System.out.println("3. Take back Your money");
+            System.out.println("4. Exit app");
+            System.out.print("Enter Your choice: ");
+            inputNumber = scanner.nextInt();
+            switch (inputNumber) {
+                case 1: {
+                    account.showAccount();
+                    break;
+                }
+                case 2: {
+                    account.depositMoney();
+                    break;
+                }
+                case 3: {
+                    account.takeMoney();
+                    break;
+                }
+                case 4: {
+                    System.out.println("We are waiting for You!");
+                    break;
+                }
             }
-            case 2: {
-                account.depositMoney();
-            }
-            case 3: {
-                account.takeMoney();
-            }
-            case 4: {
-                System.out.println("We are waiting for You!");
-            }
-        }
-
+        } while (inputNumber != 4);
     }
 }
